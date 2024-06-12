3c79700ed22b6ad6ab92ce711bfc0729016524e6
ravi.saini <=>
2024-06-12 19:27:15 +0530

    first commit

---
+++ src/test/java/com/xome/payment/PaymentApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.xome.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
