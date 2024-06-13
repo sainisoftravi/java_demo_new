fd0418241edc676145cab1e8d98b5e621d5e633f
Ravi Saini <72566431+sainisoftravi@users.noreply.github.com>
2024-06-13 17:18:18 +0530

    Update ApiService.java

--- src/main/java/com/xome/payment/config/ApiService.java
+++ src/main/java/com/xome/payment/config/ApiService.java
@@ -16,36 +16,9 @@ import java.util.Map;
 @Service
 public class ApiService {
 
-    @Value("${third.party.api.url}")
-    private String apiUrl;
-
-    @Value("${third.party.api.username}")
-    private String username;
-
-    @Value("${third.party.api.password}")
-    private String password;
-
     public String callThirdPartyApi() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        String auth = username + ":" + password;
-        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes());
-        String authHeader = "Basic " + new String(encodedAuth);
-        headers.set("Authorization", authHeader);
-
-        Map<String, Object> body = new HashMap<>();
-        Map<String, Object> header = new HashMap<>();
-        header.put("created", System.currentTimeMillis());
-        header.put("auth_handle", "xome_house_auctions");
-        body.put("header", header);
-
-        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
-
-        return response.getBody();
+        
+       
+        return "This is payment microservice."
     }
 }
