45028e3d1c3651e4f827e1a4cc326c189fa7ffab
Ravi Saini <72566431+sainisoftravi@users.noreply.github.com>
2024-06-13 19:54:47 +0530

    Update ApiService.java

--- src/main/java/com/xome/payment/config/ApiService.java
+++ src/main/java/com/xome/payment/config/ApiService.java
@@ -12,13 +12,33 @@ import org.springframework.http.MediaType;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+public class Function {
+    @FunctionName("HttpTrigger-Java")
+    public HttpResponseMessage run(
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
 
-@Service
-public class ApiService {
+        // Parse query parameter
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
 
-    public String callThirdPartyApi() {
-        
-       
-        return "This is payment microservice.";
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
     }
 }
+
+@Service
+//public class ApiService {
+
+//    public String callThirdPartyApi() {
+        
+       
+//        return "This is payment microservice.";
+  //  }
+//}
